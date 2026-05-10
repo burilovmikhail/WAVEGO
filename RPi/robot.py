7a12ef8bc8d22bcc43cb7473a4088aac3fa42657
Mikhail Burilov <burilovmikhail@gmail.com>
2026-05-10 15:31:01 +0300

    Update serial port write

--- RPi/robot.py
+++ RPi/robot.py
@@ -7,9 +7,20 @@ import json
 import serial
 
 _SERIAL_PORT = next((p for p in ('/dev/serial0', '/dev/ttyAMA0', '/dev/ttyS0') if os.path.exists(p)), '/dev/ttyS0')
+print(f'[serial] opening {_SERIAL_PORT}')
 ser = serial.Serial(_SERIAL_PORT, 115200)
+print(f'[serial] opened: {ser}')
 dataCMD = json.dumps({'var':"", 'val':0, 'ip':""})
 upperGlobalIP = 'UPPER IP'
+
+
+def _send(cmd):
+    data = json.dumps(cmd).encode()
+    print(f'[serial] >> {data}')
+    try:
+        ser.write(data)
+    except Exception as e:
+        print(f'[serial] write error: {e}')
 
 
 pitch, roll = 0, 0
@@ -20,82 +31,63 @@ def setUpperIP(ipInput):
 	upperGlobalIP = ipInput
 
 def forward(speed=100):
-	dataCMD = json.dumps({'var':"move", 'val':1})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 1})
 	print('robot-forward')
 
 def backward(speed=100):
-	dataCMD = json.dumps({'var':"move", 'val':5})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 5})
 	print('robot-backward')
 
 def left(speed=100):
-	dataCMD = json.dumps({'var':"move", 'val':2})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 2})
 	print('robot-left')
 
 def right(speed=100):
-	dataCMD = json.dumps({'var':"move", 'val':4})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 4})
 	print('robot-right')
 
 def stopLR():
-	dataCMD = json.dumps({'var':"move", 'val':6})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 6})
 	print('robot-stop')
 
 def stopFB():
-	dataCMD = json.dumps({'var':"move", 'val':3})
-	ser.write(dataCMD.encode())
+	_send({'var': "move", 'val': 3})
 	print('robot-stop')
 
-
-
 def lookUp():
-	dataCMD = json.dumps({'var':"ges", 'val':1})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 1})
 	print('robot-lookUp')
 
 def lookDown():
-	dataCMD = json.dumps({'var':"ges", 'val':2})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 2})
 	print('robot-lookDown')
 
 def lookStopUD():
-	dataCMD = json.dumps({'var':"ges", 'val':3})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 3})
 	print('robot-lookStopUD')
 
 def lookLeft():
-	dataCMD = json.dumps({'var':"ges", 'val':4})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 4})
 	print('robot-lookLeft')
 
 def lookRight():
-	dataCMD = json.dumps({'var':"ges", 'val':5})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 5})
 	print('robot-lookRight')
 
 def lookStopLR():
-	dataCMD = json.dumps({'var':"ges", 'val':6})
-	ser.write(dataCMD.encode())
+	_send({'var': "ges", 'val': 6})
 	print('robot-lookStopLR')
 
-
-
 def steadyMode():
-	dataCMD = json.dumps({'var':"funcMode", 'val':1})
-	ser.write(dataCMD.encode())
+	_send({'var': "funcMode", 'val': 1})
 	print('robot-steady')
 
 def jump():
-	dataCMD = json.dumps({'var':"funcMode", 'val':4})
-	ser.write(dataCMD.encode())
+	_send({'var': "funcMode", 'val': 4})
 	print('robot-jump')
 
 def handShake():
-	dataCMD = json.dumps({'var':"funcMode", 'val':3})
-	ser.write(dataCMD.encode())
+	_send({'var': "funcMode", 'val': 3})
 	print('robot-handshake')
 
 
@@ -118,13 +110,11 @@ def lightCtrl(colorName, cmdInput):
 		colorNum = 6
 	elif colorName == 'cyber':
 		colorNum = 7
-	dataCMD = json.dumps({'var':"light", 'val':colorNum})
-	ser.write(dataCMD.encode())
+	_send({'var': "light", 'val': colorNum})
 
 
 def buzzerCtrl(buzzerCtrl, cmdInput):
-	dataCMD = json.dumps({'var':"buzzer", 'val':buzzerCtrl})
-	ser.write(dataCMD.encode())
+	_send({'var': "buzzer", 'val': buzzerCtrl})
 
 
 
